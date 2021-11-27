b0ab32954a4b7e95cb0a9be025a664366ebd5f06
Clifford Bamert <cliff.bam92@gmail.com>
2021-11-26 18:17:57 -0600

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,6 @@ import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
-// Workaround to problem with icons being huge huge first load prior to resizing
-// This ensures that the icon CSS is loaded immediately before attempting to render icons
-// See https://github.com/FortAwesome/react-fontawesome/issues/134
-import "../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
-import { config as fontAwesomeConfig } from "@fortawesome/fontawesome-svg-core";
-// Prevent fontawesome from dynamically adding its css since we did it manually above
-fontAwesomeConfig.autoAddCss = false;
-
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
