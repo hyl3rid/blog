70c64d060df382dbed15706e6dce7945399b8221
Clifford Bamert <cliff.bam92@gmail.com>
2021-12-26 11:59:28 -0600

    commit

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,16 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 const posts = require("../../cache/data").posts;
 
-type Data = {
-  results: string[];
-};
-
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(posts);
   const results = req.query.q
     ? posts.filter((post: any) =>
         post.title.toLowerCase().includes(req.query.q)
       )
-    : [1];
+    : [];
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ results }));
